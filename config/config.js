c8419345afc98f490779d44490689ce07dbc736d
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-10 10:59:21 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,8 +9,8 @@ let config = {
       name: 'rewards-ref-app-mvc'
     },
     port: 3000,
-    CLIENT_ID:'ef6778befedb4311a4e211e75cf89f73',
-    CLIENT_SECRET:'8546b3341ebdef7953164c34a7abe66c',
+    CLIENT_ID:'',
+    CLIENT_SECRET:'',
     REDIRECT_URI:'http://localhost:3000/authredirect',  //Replace this with the redirect uri entered in the developer portal
     BASE_URI:'https://api-sandbox.capitalone.com',
     SESSION_SECRET: 'thisisnowarandomstring' //Change this to a random string
